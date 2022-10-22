af10b8f8c1ab66531cc18b6460787d20be04d75d
Yamaguchi-shuhei <81962317+Yamaguchi-shuhei@users.noreply.github.com>
2022-10-22 18:57:46 +0900

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.0",
+   defaultNetwork: "goerli",
+   networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+  },
+}
